8c7effce1c4a73bed42f088dc6969225470c1ba0
nikhil <nikhilgaikwad4691@gmail.com>
2026-09-05 19:22:33 +0530

    first commit in QA branch

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/html"
+    });
+
+    res.end(`
+        <h1>Hello from Node.js team this is cicd to automate application deployment</h1>
+        <p>Application deployed using GitHub Actions → AWS EC2</p>
+        <p>Push changes in QA-Team Branch !!!!!!  </p>
+    `);
+});
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
